b637808b70b6f4226dea719ef78c105af668f5cc
wz <wenzhe1423@gmail.com>
2025-11-14 08:15:04 +0800

    Feat: Create Postgres Table

---
+++ backend/postgres.js
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: true,
+  },
+});
+
+const client = await pool.connect();
+
+async function createTable() {
+  // Drop the table if it already exists
+  await client.query("DROP TABLE IF EXISTS userinfo;");
+
+  await client.query(`
+    CREATE TABLE user_info(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      age INT
+    );
+    `);
+
+  console.log("Created Table");
+}
+
+createTable();
